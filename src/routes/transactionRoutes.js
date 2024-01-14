f49b230cc4bdd5b72ca9971796ba9c09387ee32e
Davi Ribeiro <davi.rb@hotmail.com>
2024-01-14 18:56:59 -0300

    feat: Data validation using Joi added

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import transactionController from "../controllers/transactionController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { validationSchemaMiddleware } from "../middleware/validationSchemaMiddleware.js";
+import { CreateTransaction } from "../schemas/validation/CreateTransaction.js";
 
 const transactionRouter = Router();
 
 transactionRouter.use(authMiddleware);
 
-transactionRouter.post("/transactions", transactionController.create);
+transactionRouter.post(
+  "/transactions",
+  validationSchemaMiddleware(CreateTransaction),
+  transactionController.create
+);
 
 transactionRouter.get("/transactions", transactionController.findAllByUser);
 
