5542ff27a972f7a296efd373e8e3160f7521669c
Davi Ribeiro <davi.rb@hotmail.com>
2024-03-18 08:29:16 -0300

    feat: fix server error

--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ app.use(json());
 app.use(cors());
 app.use(authRouter);
 app.use(transactionRouter);
-disconectDb();
 
 const port = normalizePort(process.env.PORT || 3001);
 app.listen(port, () => console.log(`Server linstenig in port ${port}`));
